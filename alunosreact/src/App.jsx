60f32aa00e71c4b84f22275b6ea3e9fabb09ce42
Larksel <lemuelgames@outlook.com>
2024-04-23 14:10:14 -0300

    Recursos adicionados ao projeto

--- alunosreact/src/App.jsx
+++ alunosreact/src/App.jsx
@@ -1,3 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+import logoCadastro from './assets/cadastro.svg';
+
 export default function App() {
   return (
     <div>
