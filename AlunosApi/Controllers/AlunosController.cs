e1cd2518691d780c268daefb6e2f23f22090f512
Larksel <lemuelgames@outlook.com>
2024-04-22 23:34:58 -0300

    AlunosController criada

---
+++ AlunosApi/Controllers/AlunosController.cs
@@ -0,0 +1,13 @@
+﻿using AlunosApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlunosApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunosController(IAlunoService alunoService) : ControllerBase
+    {
+        private IAlunoService _alunoService = alunoService;
+    }
+}
