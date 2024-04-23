dd68b65745ee396717911a5ae51076afb1038ef5
Larksel <lemuelgames@outlook.com>
2024-04-23 01:10:01 -0300

    Endpoints da AlunosController implementados

--- AlunosApi/Controllers/AlunosController.cs
+++ AlunosApi/Controllers/AlunosController.cs
@@ -1,4 +1,5 @@
-﻿using AlunosApi.Services;
+﻿using AlunosApi.Models;
+using AlunosApi.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,8 +7,119 @@ namespace AlunosApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    //[Produces("application/json")] # utilizado para forçar o formato das informações retornadas
     public class AlunosController(IAlunoService alunoService) : ControllerBase
     {
         private IAlunoService _alunoService = alunoService;
+
+        [HttpGet]
+        //[ProducesResponseType(StatusCodes.Status200OK)] # utilizado para especificar os códigos que podem ser retornados
+        public async Task<ActionResult<IAsyncEnumerable<Aluno>>> GetAlunos()
+        {
+            try
+            {
+                var alunos = await _alunoService.GetAlunos();
+                return Ok(alunos);
+            }
+            catch
+            {
+                //return BadRequest("Request inválido"); # Alternativa menos verbosa
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao obter alunos");
+            }
+        }
+
+        [HttpGet("AlunoPorNome")] // Para diferenciar da rota GET acima
+        public async Task<ActionResult<IAsyncEnumerable<Aluno>>> GetAlunosByName([FromQuery] string nome)
+        {
+            try
+            {
+                var alunos = await _alunoService.GetAlunosByNome(nome);
+
+                if (alunos.Count() == 0)
+                    return NotFound($"Não existem alunos com o critério {nome}");
+
+                return Ok(alunos);
+            }
+            catch
+            {
+                return BadRequest("Request inválido");
+            }
+        }
+
+        [HttpGet("{id:int}", Name="GetAluno")] // Url = /api/Alunos/{id}
+        public async Task<ActionResult<Aluno>> GetAluno(int id)
+        {
+            try
+            {
+                var aluno = await _alunoService.GetAluno(id);
+
+                if (aluno == null)
+                    return NotFound($"O aluno com id {id} não existe");
+
+                return Ok(aluno);
+            }
+            catch
+            {
+                return BadRequest("Request inválido");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Aluno aluno)
+        {
+            try
+            {
+                await _alunoService.CreateAluno(aluno);
+                return CreatedAtRoute(nameof(GetAluno), new { id = aluno.Id }, aluno);
+            }
+            catch
+            {
+                return BadRequest("Request inválido");
+            }
+        }
+
+        [HttpPut("{id:int}")] // Sempre faz uma atualização completa
+        public async Task<ActionResult> Update(int id, [FromBody] Aluno aluno)
+        {
+            try
+            {
+                if (aluno.Id == id)
+                {
+                    await _alunoService.UpdateAluno(aluno);
+                    return Ok($"Aluno com id {id} atualizado com sucesso");
+                    //return NoContent(); 204
+                }
+                else
+                {
+                    return BadRequest("Dados inconsistentes");
+                }
+            }
+            catch
+            {
+                return BadRequest("Request inválido");
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                var aluno = await _alunoService.GetAluno(id);
+                if (aluno != null)
+                {
+                    await _alunoService.DeleteAluno(aluno);
+                    return Ok($"Aluno com id {id} deletado com sucesso");
+                }
+                else
+                {
+                    return NotFound($"Aluno com id {id} não encontrado");
+                }
+            }
+            catch
+            {
+                return BadRequest("Request inválido");
+            }
+        }
     }
 }
