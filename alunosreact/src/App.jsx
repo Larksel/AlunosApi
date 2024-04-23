ecb56aec5e91ca180144317cee506592cc9cc7d9
Larksel <lemuelgames@outlook.com>
2024-04-23 14:18:55 -0300

    Estrutura html feita

--- alunosreact/src/App.jsx
+++ alunosreact/src/App.jsx
@@ -2,11 +2,48 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 import logoCadastro from './assets/cadastro.svg';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const baseUrl = 'https://localhost:7260/api/alunos';
+  const [data, setData] = useState([]);
+
+  const pedidoGet = async () => {
+    await axios.get(baseUrl)
+    .then(res => {
+      setData(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  useEffect(() => {
+    pedidoGet();
+  }, []);
+
   return (
-    <div>
-      Hello World
+    <div className='App'>
+      <br />
+      <h3>Cadastro de Alunos</h3>
+      <header>
+        <img src={logoCadastro} alt="Cadastro" />
+        <button className='btn btn-success'>Incluir Novo Aluno</button>
+      </header>
+      <table className='table table-bordered'>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Idade</th>
+            <th>Operação</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
     </div>
   )
 }
